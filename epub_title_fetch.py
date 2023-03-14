63b50ed14331bd39ab125184311a5de10f3807ce
Kavish Gour <kavishgr@protonmail.com>
2023-03-14 21:34:23 +0400

    Update epub_title_fetch.py

--- epub_title_fetch.py
+++ epub_title_fetch.py
@@ -8,8 +8,8 @@ for i in os.listdir():
         book = book.get_metadata('DC', 'title')
         contains_title = (book[0])
         book = contains_title[0]
-# if book contains kavish hunter then replace
-# if not just rename
+# if filename contains "(for Kavish Gour)",
+# remove it from the filename
         book = book.replace(" (for Kavish Gour)", '.epub')	
         print(f"{i} -> {book}")
         os.rename(i, book)
