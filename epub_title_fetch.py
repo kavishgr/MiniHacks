12df1a85649024b09f915ae1547fcfd032bcadde
Kavish Gour <kavishgr@protonmail.com>
2023-04-04 17:49:44 +0400

    Update epub_title_fetch.py

--- epub_title_fetch.py
+++ epub_title_fetch.py
@@ -1,17 +1,32 @@
-import ebooklib
 from ebooklib import epub
-import os
+import os, re
 
-for i in os.listdir():
-    if i.endswith("epub"):
-        book = epub.read_epub(i)
-        book = book.get_metadata('DC', 'title')
-        contains_title = (book[0])
-        book = contains_title[0]
-# if filename contains "(for Kavish Gour)",
-# remove it from the filename
-        book = book.replace(" (for Kavish Gour)", '.epub')	
-        print(f"{i} -> {book}")
-        os.rename(i, book)
+#string to remove from filename
+#pattern = re.compile(r"\(for Josianne Machongpi\)")
 
-print("DONE")
+# if there are multiples patterns
+# make a list
+todel = "("text_to_remove_from_title")"
+
+for file in os.listdir():
+    if file.endswith(".epub") and not file.startswith('.DS_Store'):
+        try:
+            epubReader = epub.read_epub(file)
+            book = epubReader.get_metadata('DC', 'title')
+            title = book[0][0]
+            if todel in title:
+                newtitle = title.replace(todel, "")
+                newtitle = newtitle.rstrip(" ")
+                newtitle = f"{newtitle}.epub"
+                if "/" in newtitle:
+                    newtitle = newtitle.replace("/", "-") 
+                print(f"Renaming {file} to {newtitle}")
+                os.rename(file, newtitle)
+            else:
+                os.rename(file, title)
+        except KeyError:
+            continue
+        except epub.EpubException as e:
+            print(f"Error reading {file}: {e}")
+            continue
+
